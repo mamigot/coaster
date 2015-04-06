43ff31e93a854a374fbc2850339198ee9d4ef68d
miguel5 <miguel.amigot@gmail.com>
2015-04-05 23:34:15 -0400

    Inheritance-based items for Scrapy

--- content_acquisition/scraper/restaurantbot/items.py
+++ content_acquisition/scraper/restaurantbot/items.py
@@ -1,16 +1,31 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 from scrapy.item import Item, Field
 
 
 class RestaurantItem(Item):
+    '''
+    Information that *every* listing service should have about a restaurant
+    '''
     name = Field()
     cuisine = Field()
-    location = Field()
-    hours = Field()
-    menu_href = Field()
+    description = Field()
+    pricing = Field()
+
+    street_address = Field()
+    addressLocality = Field()
+    addressRegion = Field()
+    postalCode = Field()
+
+
+class NYMagRestaurantItem(RestaurantItem):
+    '''
+    Information provided by NYMag
+    '''
+    nymag_href = Field()
+    official_href = Field()
+
+    neighborhood = Field()
+    latitude = Field()
+    longitude = Field()
+
+    avg_reader_rating = Field()
+    number_reader_reviews = Field()
