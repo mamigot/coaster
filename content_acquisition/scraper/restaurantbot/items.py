84a3d65512b8e35fb6f6e27f8d46d54b51916064
miguel5 <miguel.amigot@gmail.com>
2015-04-05 23:49:22 -0400

    Base class for review items and NYMag's child

--- content_acquisition/scraper/restaurantbot/items.py
+++ content_acquisition/scraper/restaurantbot/items.py
@@ -3,7 +3,7 @@ from scrapy.item import Item, Field
 
 class RestaurantItem(Item):
     '''
-    Information that *every* listing service should have about a restaurant
+    Information that *every* listing service should contain about a restaurant
     '''
     name = Field()
     cuisine = Field()
@@ -16,9 +16,17 @@ class RestaurantItem(Item):
     postalCode = Field()
 
 
+class ReviewItem(Item):
+    '''
+    Information that *every* review should contain
+    '''
+    title = Field()
+    text = Field()
+
+
 class NYMagRestaurantItem(RestaurantItem):
     '''
-    Information provided by NYMag
+    Information provided by NYMag's listing
     '''
     nymag_href = Field()
     official_href = Field()
@@ -29,3 +37,17 @@ class NYMagRestaurantItem(RestaurantItem):
 
     avg_reader_rating = Field()
     number_reader_reviews = Field()
+
+
+class NYMagReviewItem(ReviewItem):
+    '''
+    Information provided by NYMag's reviews
+    '''
+    would_go_back = Field()
+    overall_rating = Field()
+    food_rating = Field()
+    service_rating = Field()
+    decor_rating = Field()
+
+    review_helpfulness_rating = Field()
+    number_helpfulness_evaluations = Field()
