97841542b8db0a11e1f76a72db07f13b3efbc51b
miguel5 <miguel.amigot@gmail.com>
2015-04-12 22:29:34 -0400

    Unnecessary function

--- utils/sql/handlers.py
+++ utils/sql/handlers.py
@@ -2,14 +2,6 @@
 Utility functions for access to Postgres
 '''
 
-def get_column_value(session, row_object, property_name):
-    '''
-    Gets value of property from model instance corresponding to a
-    given row.
-    '''
-    pass
-
-
 def get_row(session, model, model_field, value):
     '''
     Gets object of row that matches the given value. If not present,
