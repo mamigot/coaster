77ffb6479f3e9745d2583b8fe8d690e9e528791c
miguel5 <miguel.amigot@gmail.com>
2015-04-16 20:20:11 -0400

    Specified pipelines on settings

--- content_acquisition/scraper/edx_bot/settings.py
+++ content_acquisition/scraper/edx_bot/settings.py
@@ -8,5 +8,6 @@ NEWSPIDER_MODULE = 'edx_bot.spiders'
 # http://doc.scrapy.org/en/latest/topics/item-pipeline.html#activating-an-item-pipeline-component
 ITEM_PIPELINES = {
     'edx_bot.pipelines.course_placement.CoursePlacement': 100,
-    #'edx_bot.pipelines.content_placement.ContentPlacement': 150,
+    'edx_bot.pipelines.content_placement.ContentPlacement': 150,
+    'edx_bot.pipelines.youtube_data_placement.YouTubeDataPlacement': 200,
 }
