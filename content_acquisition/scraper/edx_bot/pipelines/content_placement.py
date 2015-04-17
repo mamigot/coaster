527e98e943261d30bcd7e4a22ca2f855832a40d1
miguel5 <miguel.amigot@gmail.com>
2015-04-16 20:19:20 -0400

    Note crawl time

--- content_acquisition/scraper/edx_bot/pipelines/content_placement.py
+++ content_acquisition/scraper/edx_bot/pipelines/content_placement.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import re
 
 from scrapy import log
@@ -38,6 +39,8 @@ class ContentPlacement(object):
 
         for item_section in item['sections']:
             self.process_section(item_section, course.sections)
+
+        course.latest_crawled_on = datetime.utcnow()
 
         self.session.add(course)
         self.session.commit()
