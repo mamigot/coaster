db69645b5069f2e3935be90bcbb248a2a0372d17
miguel5 <miguel.amigot@gmail.com>
2015-04-25 22:08:29 -0400

    Useful commands

---
+++ useful_commands.sh
@@ -0,0 +1,12 @@
+# Generate SSH key
+ssh-keygen -R 104.131.110.156
+
+# Sync files across systems
+rsync -aP --update Coaster/ root@104.131.110.156:/home/mikel/coaster/
+
+# View nginx's error log
+cat /var/log/nginx/error.log
+
+# List uwsgi processes
+ps ax | grep uwsgi
+# Once their PIDs are known, kill them by typing "kill PID"
