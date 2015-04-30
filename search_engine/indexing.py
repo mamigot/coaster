f0fe5071d71b939f844a2188006d008de70bb01d
miguel5 <miguel.amigot@gmail.com>
2015-04-30 11:02:53 -0400

    Clarified comments, added support for the wd structure and improved the efficiency of get_number_of_terms_in_collection()

--- search_engine/indexing.py
+++ search_engine/indexing.py
@@ -5,30 +5,32 @@ from redis.exceptions import ResponseError
 from search_engine.nlp import tokenizer, normalize_token, is_valid_term
 
 '''
-Three underlying types of data structure in Redis for each document
-type, as in video_transcripts, etc.:
-    - (fdt) Sorted sets to record the frequency of a term t in a document d:
-        - The name of the structure combines the collection type as well as
-        the term. The keys of the sorted set are the document IDs and the
-        scores (or values) are the frequencies of the term in the document.
-        - ex.: "video_transcripts:my_term 4 7" means that there are 4 instances
-        of "my_term" in document 7.
-    - (ft) Hashes to record the number of documents containing a term t:
-        - The name of the structure is essentially the collection type,
-        the keys are the terms and the values are the number of documents
-        containing them.
-    - (s) Hashes to signal whether a document has been fully indexed:
-        - Once a document is fully indexed, a flag is set on Redis to signal
-        that said document does not need to be indexed again.
+Following underlying types of data structure in Redis for each document
+type required to perform the TDxIDF calculations (see below for naming
+conventions):
+
+    - (fdt) Sorted sets to record the frequency of a term t in a document d.
+
+    - (ft)  Hashes to record the number of documents containing a term t.
+
+    - (wd)  Hashes to record the magnitude of the vector of weights of
+            the terms in a document.
+
+    - (s)   Hashes to signal whether a document has been fully indexed:
+
 
 Relevant collection kinds (collections whose documents are indexed):
     - video_transcripts
 '''
 
+
 def fdt_name(collection_kind, term):
     '''
     Returns the name of the fdt structure (see above) for the given
     collection kind.
+
+    - ex.: "video_transcripts:my_term 4 7"
+        - there are 4 instances of "my_term" in document 7.
     '''
     return "fdt:" + collection_kind + ":" + term
 
@@ -41,10 +43,21 @@ def ft_name(collection_kind):
     return "ft:" + collection_kind
 
 
+def wd_name(collection_kind):
+    '''
+    Returns the name of the wd structure (see above) for the given
+    collection kind.
+    '''
+    return "wd:" + collection_kind
+
+
 def s_name(collection_kind):
     '''
     Returns the name of the s structure (see above) for the given
     collection kind.
+
+    Once a document is fully indexed, a flag is set on Redis to signal
+    that said document does not need to be indexed again.
     '''
     return "s:" + collection_kind
 
@@ -134,5 +147,8 @@ def get_number_of_terms_in_collection(collection_kind):
     '''
     Get the total number of terms in the given collection.
     '''
-    term_frequencies = [int(v) for v in redis.hvals(ft_name(collection_kind))]
-    return sum(term_frequencies)
+    frequencies = 0
+    for v in redis.hvals(ft_name(collection_kind)):
+        frequencies += int(v)
+
+    return frequencies
