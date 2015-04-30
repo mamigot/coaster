1192abf8ee58068bd0265f2c2fb7f698a3718ac1
miguel5 <miguel.amigot@gmail.com>
2015-04-29 23:47:06 -0400

    Refactoring --improved way to access Redis structures

--- search_engine/indexing.py
+++ search_engine/indexing.py
@@ -2,71 +2,82 @@ from utils.sql import get_session
 from utils.sql.models.course_video import CourseVideo
 from utils.redis import redis
 from redis.exceptions import ResponseError
-
 from search_engine.nlp import tokenizer, normalize_token, is_valid_term
 
+'''
+Three underlying types of data structure in Redis for each document
+type, as in video_transcripts, etc.:
+    - (fdt) Sorted sets to record the frequency of a term t in a document d:
+        - The name of the structure combines the collection type as well as
+        the term. The keys of the sorted set are the document IDs and the
+        scores (or values) are the frequencies of the term in the document.
+        - ex.: "video_transcripts:my_term 4 7" means that there are 4 instances
+        of "my_term" in document 7.
+    - (ft) Hashes to record the number of documents containing a term t:
+        - The name of the structure is essentially the collection type,
+        the keys are the terms and the values are the number of documents
+        containing them.
+    - (s) Hashes to signal whether a document has been fully indexed:
+        - Once a document is fully indexed, a flag is set on Redis to signal
+        that said document does not need to be indexed again.
+'''
 
-def get_term_frequency_in_document(collection_kind, term, doc_ID):
+def fdt_name(collection_kind, term):
     '''
-    Gets the frequency of the given term in the given document.
-
-    ex.: "zscore ii_video_transcripts:hotel 7" for the following values:
-        collection_kind = "ii_video_transcripts"
-        doc_ID = 7
-        term = "hotel"
+    Returns the name of the fdt structure (see above) for the given
+    collection kind.
     '''
-    collection = "%s:%s" % (collection_kind, term)
-    return redis.zscore(collection, doc_ID)
+    return "fdt:" + collection_kind + ":" + term
 
 
-def get_number_of_documents_containing_term(collection_kind, term):
+def ft_name(collection_kind):
     '''
-    Gets the number of documents containing the given term.
-
-    ex.: "hvals ttc_video_transcripts hotel"
+    Returns the name of the ft structure (see above) for the given
+    collection kind.
     '''
-    return redis.hmget(collection_kind, term)
+    return "ft:" + collection_kind
 
 
-def get_number_of_terms_in_collection(collection_kind):
+def s_name(collection_kind):
     '''
-    Get the total number of terms in the given collection.
-
-    ex.: "hvals ttc_video_transcripts"
+    Returns the name of the s structure (see above) for the given
+    collection kind.
     '''
-    return sum([int(v) for v in redis.hvals(collection_kind)])
+    return "s:" + collection_kind
 
 
 def index_video_transcripts():
     session = get_session()
+    collection = "video_transcripts"
 
     for c in session.query(CourseVideo).filter(CourseVideo.transcript != None):
         # Only proceed if the document has not been fully indexed
-        if redis.hmget("fi_video_transcripts", c.id)[0] == None:
+        if not document_has_been_fully_indexed(s_name(collection), c.id):
             print "Building inverted index for video transcript with id=%d..." % c.id
 
             transcript = c.transcript.lower()
             frequencies = determine_frequencies_of_valid_terms(transcript)
 
             for term in frequencies.keys():
-                # print "indexing: (term, frequency) = (%s, %d)" % (term, frequencies[term])
-                redis.zadd("ii_video_transcripts:%s" % term, c.id, frequencies[term])
-                redis.hincrby("ttc_video_transcripts", term, frequencies[term])
+                print "indexing: (term, frequency) = (%s, %d)" % (term, frequencies[term])
+                redis.zadd(fdt_name(collection, term), c.id, frequencies[term])
+                redis.hincrby(ft_name(collection), term, 1)
 
-            # Signal that the document has been fully indexed
-            redis.hmset("fi_video_transcripts", {c.id: 1})
+            signal_full_indexing_of_document(s_name(collection), c.id)
 
+            try:
+                # Blocking operation to ensure that the data is written to disk.
+                # If we try to save while Redis is already saving, then ignore.
+                redis.bgsave()
+                redis.save()
+            except ResponseError, e:
+                if "Background save already in progress" not in e:
+                    session.close()
+                    raise
         else:
             print "Already considered. Skipping video transcript with id=%d..." % c.id
 
-        # Blocking operation to ensure that the data is written to disk.
-        # If we try to save while Redis is already saving, then ignore.
-        try:
-            redis.save()
-        except ResponseError, e:
-            if e != "Background save already in progress":
-                session.close()
-                raise
+        return
 
     session.close()
 
@@ -84,3 +95,40 @@ def determine_frequencies_of_valid_terms(text):
                 frequencies[token] = 1
 
     return frequencies
+
+
+def document_has_been_fully_indexed(collection_kind, doc_ID):
+    '''
+    Returns True if the signal that the document has been fully indexed
+    is set for a document of a given ID.
+
+    ex.: "hmget fi_video_transcripts 7"
+    '''
+    return '1' in redis.hmget(collection_kind, doc_ID)
+
+
+def signal_full_indexing_of_document(collection_kind, doc_ID):
+    redis.hmset(collection_kind, {doc_ID: 1})
+
+
+def get_frequency_of_term_in_document(collection_kind, term, doc_ID):
+    '''
+    Gets the frequency of the given term in the given document.
+    '''
+    collection = fdt_name(collection_kind, term)
+    return redis.zscore(collection, doc_ID)
+
+
+def get_number_of_documents_containing_term(collection_kind, term):
+    '''
+    Gets the number of documents containing the given term.
+    '''
+    return redis.hmget(ft_name(collection_kind), term)
+
+
+def get_number_of_terms_in_collection(collection_kind):
+    '''
+    Get the total number of terms in the given collection.
+    '''
+    term_frequencies = [int(v) for v in redis.hvals(ft_name(collection_kind))]
+    return sum(term_frequencies)
--- utils/redis/__init__.py
+++ utils/redis/__init__.py
@@ -3,36 +3,6 @@ import redis
 redis = redis.Redis(host='localhost', port=6379)
 
 
-'''
-Sorted sets containing the inverted indexes for each term
-ex.:
-    "ii_video_transcripts:quicksort" is the name, the keys
-    are document IDs and the scores are the frequencies of
-    the terms in said documents.
-'''
-inverted_index_sorted_sets = [
-    'ii_video_transcripts',
-]
-
-'''
-Hashes containing the total term counts.
-ex.:
-    "ttc_video_transcripts" is the name of the hash, the
-    keys are the terms present in the video transcripts
-    and the values are the total frequencies of said terms.
-'''
-total_term_counts_hashes = [
-    'ttc_video_transcripts',
-]
-
-'''
-Hashes signaling which documents in redis have been fully indexed.
-ex.:
-    "fi_video_transcripts",
-'''
-fully_indexed_signal_hashes = [
-    'fi_video_transcripts',
-]
 
 '''
 REDIS COMMANDS:
