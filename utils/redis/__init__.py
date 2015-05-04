df32a5355add10eac40bb8ad29d5ec4c29175966
miguel5 <miguel.amigot@gmail.com>
2015-05-04 10:55:35 -0400

    Remove didactic commands

--- utils/redis/__init__.py
+++ utils/redis/__init__.py
@@ -1,28 +1,3 @@
 import redis
 
 redis = redis.Redis(host='localhost', port=6379)
-
-
-
-'''
-REDIS COMMANDS:
----------------
-
-
-SORTED SET
---------------
-for a sorted set "friends"...
-zadd friends 3 "bob" ---> add key "bob" with score "3"
-zscore friends bob ---> 3
-zincrby friends 1 "pepe" --> creates "pepe" key if it doesn't exist and sets to 1
-
-
-HASH
---------------
-for a hash "papers"...
-hmset papers "yeygeniv" 3
-hmget papers "yeygeniv" ---> 3
-hlen papers ---> number of keys in "papers"
-hincrby papers "yeygeniv" 5 ---> increment key "yeygeniv" by 5
-hvals papers
-'''
