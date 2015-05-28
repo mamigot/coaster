6e647df02e3e8b0538271691304b20ccdc203ddd
miguel5 <miguel.amigot@gmail.com>
2015-05-28 02:22:20 +0200

    Rename pipeline to match spider's name

--- scraper/edx_bot/settings.py
+++ scraper/edx_bot/settings.py
@@ -7,7 +7,7 @@ NEWSPIDER_MODULE = 'scraper.edx_bot.spiders'
 ITEM_PIPELINES = {
     'scraper.edx_bot.pipelines.course_list_insertion.CourseListInsertion': 100,
     'scraper.edx_bot.pipelines.general_course_content_insertion.GeneralCourseContentInsertion': 150,
-    'scraper.edx_bot.pipelines.video_transcript_insertion.VideoTranscriptInsertion': 200,
+    'scraper.edx_bot.pipelines.video_transcripts_insertion.VideoTranscriptInsertion': 200,
     'scraper.edx_bot.pipelines.youtube_stats_insertion.YouTubeStatsInsertion': 250,
 }
 
