3df2b199fb4cfd6dc594fcd096275437e81ecf56
miguel5 <miguel.amigot@gmail.com>
2015-05-28 18:14:47 +0200

    Introduce custom exceptions for added clarity

---
+++ scraper/edx_bot/exceptions.py
@@ -0,0 +1,26 @@
+class CourseNotInEnglish(Exception):
+    pass
+
+class TroubleEnrollingInCourse(Exception):
+    pass
+
+class TroubleOpeningCourse(Exception):
+    pass
+
+class TroubleCrawlingCourse(Exception):
+    pass
+
+class TroubleCrawlingSection(Exception):
+    pass
+
+class TroubleCrawlingSubsection(Exception):
+    pass
+
+class TroubleCrawlingUnit(Exception):
+    pass
+
+class TroubleCrawlingVideo(Exception):
+    pass
+
+class TroubleFetchingTranscript(Exception):
+    pass
